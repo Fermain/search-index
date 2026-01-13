b093e2369feca4d11da1a2a3bb08cdc082657005
Fermain <Fermain@users.noreply.github.com>
2026-01-13 15:20:01 +0200

    Update version to 0.3.1

--- search-index.php
+++ search-index.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Search Index
  * Description: Generates a minimal JSON search index for posts under uploads/search/index.json.
- * Version: 0.3.0
+ * Version: 0.3.1
  * Author: Your Team
  */
 
